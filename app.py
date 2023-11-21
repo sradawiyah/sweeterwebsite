58b8bcffaa66420cbf5cf24cb06a6fb6284b6b78
sradawiyah <sradawiyah27@gmail.com>
2023-11-21 07:47:52 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from pymongo import MongoClient
 import jwt
 import datetime
@@ -7,15 +10,21 @@ from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
 app = Flask(__name__)
+
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config["UPLOAD_FOLDER"] = "./static/profile_pics"
 
 SECRET_KEY = "SPARTA"
-
-MONGODB_CONNECTION_STRING = "mongodb+srv://sradawiyah27:akuobi123@cluster0.j1o6zjr.mongodb.net/"
-client = MongoClient(MONGODB_CONNECTION_STRING)
-db = client.dbsparta_plus_week4
 
 
 @app.route("/")
